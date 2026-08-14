7504cdcef9e77a41ae75c87b0c9451e3a007cd24
tomihq <hernandeztomas584@gmail.com>
2026-08-14 14:15:11 -0300

    add: fork() example, getppid() and child guardarails

---
+++ own/processes/hello-fork.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(){
+    pid_t pid = getpid();
+    printf("Hallo, meine pid ist: %d \n", pid);
+    
+    pid_t child = fork();
+    if(child < 0){
+        printf("Nein! Etwas ist schiefgelaufen \n");
+        exit(EXIT_FAILURE);
+    }
+
+    // Note that after the fork(), both processes start from the same IP.
+    if(child == 0){
+        printf("Hallo! Dies ist ein Kindprozess, und ich habe die PID: %d und meine Dad ist PID: %d \n", getpid(), getppid());
+    }else{
+        printf("Hallo! Das ist Dad.\n");
+    }
+    return 0;
+}
