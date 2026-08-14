cc8906db77345571e18fdbb4e7ee0c5d87c3804b
tomihq <hernandeztomas584@gmail.com>
2026-08-14 14:18:42 -0300

    chore: some clarification about execution order

--- own/processes/hello-fork.c
+++ own/processes/hello-fork.c
@@ -12,7 +12,7 @@ int main(){
         exit(EXIT_FAILURE);
     }
 
-    // Note that after the fork(), both processes start from the same IP.
+    // Note that after the fork(), both processes start from the same IP. We don't have a guarantee about the execution order. Consider these problems as independent.
     if(child == 0){
         printf("Hallo! Dies ist ein Kindprozess, und ich habe die PID: %d und meine Dad ist PID: %d \n", getpid(), getppid());
     }else{
