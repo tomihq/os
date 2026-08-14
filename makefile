e56abd24dc4b4064fa3a02a4eea06793384ed021
tomihq <hernandeztomas584@gmail.com>
2026-08-14 14:31:47 -0300

    add: orphan processes

--- makefile
+++ makefile
@@ -1,6 +1,6 @@
 CC = gcc
 CFLAGS = -Wall -Wextra -pedantic -g
-TARGET = hello-fork
+TARGET = hello-orphan
 VPATH = $(shell find . -type d | tr '\n' ':')
 
 all: $(TARGET)
--- own/processes/hello-orphan.c
+++ own/processes/hello-orphan.c
@@ -1,3 +1,7 @@
+/* An orphan process is a child process whose parent terminates before the child finishes executing. As a result, the child becomes an orphan.
+    When this happens, the orphan process is immediately adopted by another process, typically systemd (or PID 1) on modern Linux systems, which becomes its new parent.
+*/
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -15,8 +19,13 @@ int main(){
     // Note that after the fork(), both processes start from the same IP. We don't have a guarantee about the execution order. Consider these problems as independent.
     if(child == 0){
         printf("Hallo! Dies ist ein Kindprozess, und ich habe die PID: %d und meine Parent ist PID: %d \n", getpid(), getppid());
+        sleep(20);
     }else{
         printf("Hallo! Das ist Parent.\n");
+        exit(EXIT_SUCCESS);
     }
-    return 0;
+
+    printf("Hallo! Meine Parent ist PID: %d \n", getppid());
+    return 0; 
+
 }
