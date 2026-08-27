1785356addd93c3ff9bdd2db88de5b7150283cb8
tomihq <hernandeztomas584@gmail.com>
2026-08-27 14:40:13 -0300

    add: descriptors, pipe creation and close descriptors that wont be used by child to guarantee EOF

--- guides/one/15.c
+++ guides/one/15.c
@@ -15,20 +15,32 @@ void execute_child_2(){
 }
 
 int main() {
+    
+    int descriptores[2]; //0: lectura, 1: escritura
+    pipe(descriptores);
+
     pid_t child_1 = fork(); 
+    
     if(child_1 == 0){
-        execute_child_1();
+        //escribe
+        //garantizo que el child_1 no tenga acceso al descriptor de lectura.
+        close(descriptores[0]);
+        execute_child_1(descriptores[1]);
+
+        exit(EXIT_SUCCESS);
     }
-    //garantizar que el child_1 nunca llegue acá.
 
     pid_t child_2 = fork();
     if(child_2 == 0){
-        execute_child_2();
+        //lee
+        //garantizo que el child_2 no tenga acceso al descriptor de escritura.
+        close(descriptores[1]);
+        execute_child_2(descriptores[0]);
+        exit(EXIT_SUCCESS);
     }
 
     wait(NULL);
     wait(NULL);
 
-    exit(EXIT_SUCCESS);
-
+    return 0; 
 }
