79d381b3d8b8092a5f76ca796cee69c4c9f2e157
tomihq <hernandeztomas584@gmail.com>
2026-08-27 14:52:18 -0300

    feat: srp

--- guides/one/15.c
+++ guides/one/15.c
@@ -1,46 +1,66 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
 #include <unistd.h>
 #include <sys/wait.h>
 
-void execute_child_1(){
-    while(1){
-
-    }
+void execute_child_1(int descriptor_escritura) {
+    dup2(descriptor_escritura, STDOUT_FILENO);
+    close(descriptor_escritura);
+    execl("/bin/sh", "sh", "-c", "ls", (char *)NULL); //si esto funciona NUNCA debería pasar a lo de abajo.
+    
+    perror("execl child 1");
 }
 
-void execute_child_2(){
+void execute_child_2(int descriptor_lectura) {
+    dup2(descriptor_lectura, STDIN_FILENO);
+    close(descriptor_lectura);
+    execl("/bin/sh", "sh", "-c", "wc -l", (char *)NULL); //si esto funciona NUNCA debería pasar a lo de abajo.
 
+    perror("execl child 2");
 }
 
 int main() {
-    
-    int descriptores[2]; //0: lectura, 1: escritura
-    pipe(descriptores);
 
-    pid_t child_1 = fork(); 
-    
-    if(child_1 == 0){
-        //escribe
-        //garantizo que el child_1 no tenga acceso al descriptor de lectura.
+    int descriptores[2]; // [0]: lectura, [1]: escritura
+
+    if (pipe(descriptores) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+    pid_t child_1 = fork();
+
+    if (child_1 < 0) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (child_1 == 0) {
+        // Hijo 1 solo escribe. Cierro descriptor de lectura (ayuda a garantizar EOF)
         close(descriptores[0]);
         execute_child_1(descriptores[1]);
-
-        exit(EXIT_SUCCESS);
+        exit(EXIT_FAILURE);
     }
 
     pid_t child_2 = fork();
-    if(child_2 == 0){
-        //lee
-        //garantizo que el child_2 no tenga acceso al descriptor de escritura.
-        close(descriptores[1]);
-        execute_child_2(descriptores[0]);
-        exit(EXIT_SUCCESS);
+
+    if (child_2 < 0) {
+        perror("fork");
+        exit(EXIT_FAILURE);
     }
 
-    wait(NULL);
-    wait(NULL);
+    if (child_2 == 0) {
+        // Hijo 2 solo lee. 
+        close(descriptores[1]);
+        execute_child_2(descriptores[0]);
+        exit(EXIT_FAILURE);
+    }
 
-    return 0; 
+    close(descriptores[0]);
+    close(descriptores[1]);
+
+    waitpid(child_1, NULL, 0);
+    waitpid(child_2, NULL, 0);
+
+    return 0;
 }
